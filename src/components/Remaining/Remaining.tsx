3bf050407084639abb775d9c5783ae572621470b
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-04-15 20:23:44 +0300

    feat(project-4): add overspending

--- src/components/Remaining/Remaining.tsx
+++ src/components/Remaining/Remaining.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyledRemaining } from "./styles";
+import { Overspending, StyledRemaining } from "./styles";
 import { UseExpensesContext } from "../../context/ExpensesContext/ExpensesContext";
 import { useBudgetContext } from "../../context/BudgetContext/BudgetContext";
 
@@ -7,10 +7,25 @@ export const Remaining = () => {
   const { expenses } = UseExpensesContext();
   const { budget } = useBudgetContext();
   const [remaining, setRemaining] = useState(0);
+  const [positiveSaldo, setPositiveSaldo] = useState(true);
+
+  useEffect(() => {
+    if (remaining >= 0) {
+      setPositiveSaldo(true);
+    }
+    if (remaining < 0) {
+      setPositiveSaldo(false);
+    }
+  }, [remaining]);
 
   useEffect(() => {
     setRemaining(budget - expenses.reduce((spent, expense) => spent + Number(expense.cost), 0));
   }, [expenses, budget]);
 
-  return <StyledRemaining>Remaining: {remaining}</StyledRemaining>;
+  return (
+    <>
+      {positiveSaldo && <StyledRemaining>Remaining: {remaining}</StyledRemaining>}
+      {positiveSaldo || <Overspending>Overspending by $100 {remaining * -1}</Overspending>}
+    </>
+  );
 };
--- src/components/Remaining/styles.ts
+++ src/components/Remaining/styles.ts
@@ -11,3 +11,16 @@ export const StyledRemaining = styled.div`
   padding: 20px;
   margin: 20px 0;
 `;
+
+export const Overspending = styled.div`
+  display: flex;
+  height: 100px;
+  background: #ff0000;
+  color: #ffffff;
+  border-radius: 10px;
+  font-size: 20px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  margin: 20px 0;
+`;
